f387957a1f43dcc58d38edb014474c1a5d9d8700
Jakub Krol <jakub@jjkrol.pl>
2011-12-13 13:43:01 +0100

    ARKO lab3: wersja po laborkach

--- ARKO/projekt.s
+++ ARKO/projekt.s
@@ -1,10 +1,9 @@
 	.data
 hello:	.asciiz "Wyszukiwanie\n"
 input:	.asciiz "dane.dat"
-sig:	.word 0xFa 0xfb 0x45 0x58
-	.word 0x88
-sigsize:.word	10	
-bytemask:.byte	0
+sig:	.word 0x6E235423
+	.word 0x12345466	
+sigsize:.word	48		
 buffer:	.space	2421	
 bajt:	.asciiz "Znaleziono tyle bitow od poczatku:\n"
 
@@ -14,117 +13,93 @@ bajt:	.asciiz "Znaleziono tyle bitow od poczatku:\n"
 
 
 findsig:
+	li	$s6, 0
+	li	$t4, 0
 	move	$t1, $a0 	#temp dla buf
 	move	$t2, $a1 	#temp dla sig
-	move	$t5, $a1	#pamietaj ktory jest pierwszy bajt sygnatury
-	move	$t3, $a2 	#maska bitowa
-	move	$s3, $ra 	#temp dla return
+	lw	$t5, ($a2)	# dlugosc sygnatury
+	sub	$t5, $t5, 32
+	li	$s3, 0xFFFFFFFF	# zaladuj jedynki
+	srl	$s3, $s3, $t5	# zrob (siglen-32) zer w masce
+	
+	
+	
+
+	lw	$s1, ($t2)	# pierwsze 4  bajty sygnatury
+	addi	$t2, 4		# przesun
+	lw	$s2, ($t2)	# zaladuj drugie 4 bajty sygnatury
+	or	$s2, $s2, $s3	# utnij drugie slowo sygnatury do dodlugosci
+	# maska dl sygnatury
+
+	lw	$s4, ($t1)	# zaladuj pierwsze 4 bajty bufora 
+	addi	$t1, 4		# przesun
+	lw	$s5, ($t1)	# zaladuj kolejne
+	addi	$t1, 4		# przesun
+	lw	$s6, ($t1)	# zaladuj kolejne	
+	
+	li	$t8, 0		# zeruj licznik przesuniecia
 
 loop:
 	# t1 to wskaznik na bufor
 	# t2 to wskazanie na sygnature
-	# t3 na poczatku sprawdza czy ciag sie nie skonczyl, a potem jest maska bitowa
+	# t3 na poczatku sprawdza czy ciag sie nie skonczyl
 	# t4 licznik przesuniecia sygnatury
-	# t5
-	# t6 zapamietuje w ktorym bajcie byl pierwszy sukces
-	# t7 zapamietuje w ktorym bicie tego bajtu byl sukces
-	# s1 to bajt bufora
-	# s2 to bajt sygnatury
-	# s3 to temp dla buf AND maska
-	# s4 to temp dla sig AND maska
-	# s5 to kolejny licznik, liczy w ktorym miejscu bajtu jestesmy
-	# s6 to licznik dopasowania (czyli nty dopasowany bit)
+	
+	# t8 licznik przesuniecia
+	# t9 temp dla
+
+	# s1 to 1 slowo sygnatury
+	# s2 drugie slowo sygnatury
+	# s3 to maska dlugoscisygatury
+	# s4 pierwsze slowo bufora
+	# s5 drugie slowo bufora
+	# s6 trzecies slowo bufora
+	# s7 temp dla drugiego slowa 
 	
 
 	# czy to nie koniec ciagu przeszukiwanego?
-	lbu 	$t3, ($t1)	# zaladuj bit do porownania
-	beq	$t3, 0, loopend # salto
+	lb	$t6, ($t1)
+	beq	$t6, 0, loopend
+	bne 	$s1, $s4, nextloop	#jesli rozni sie pierwze slowo
+	move 	$s7, $s5		# tempdla $s5
+	or 	 $s5, $s5, $s3		# utnij s5
+	bne	$s5, $s3, nextloop	#drugie slowo rowne
+	b success
 
-	lbu 	$s1, ($t1)	#laduj bit bufora 
-	lbu	$s2, ($t2)	#laduj bit sygnatury 
-	sllv	$s2, $s2, $t4	# przesun w lewo o tyle ile juz jest dopasowanych
-				# (przy ciagu dalszym dopasowania)
-	li	$t3, 128	# resetuj maske bitowa, niech bedzie 10000000
-	li	$s5, 0		# koljeny licznik, liczy ile bitow
-byteloop:
-	# tutaj przesuwamy sie w obrebie jednego bajtu
-	# robimy to przesuwajac maske bitowa (w ten sposob
-	# bierzemy po jednym bicie) za pomoca srlv
+nextloop:	# nie udalo sie, przesun o jeden
+	sll	$s4, $s4, 1	# przesun pierwsze slowo bufora o jeden	
+	move	$t9, $s5	#temp
+	srl	$t9, $t9, 7	# wez ostatni
+	add	$s4, $s4, $t9	# wpisz pierwszy z s5 jako ostatni w s4
 
-	beqz	$t3, byteloopend #jesli koniec bajtu, to koncz petle
+	sll	$s5, $s5, 1
+	move	$t9, $s6	# temp
+	srl	$t9, $t9, 7	# wez ostatni
+	add	$s5, $s5, $t9	# wpisz pierwszy z s6 jako ostatni w s5
 
-	and	$s3, $t3, $s1	# sprawdz bit w buf
-	and	$s4, $t3, $s2	# sprawdz bit w sig
-	# xor dla takich samych wartosci da 0
-	beq	$s3, $s4, bytesuccess# jesli takie same, ???, PROFIT!
+	sll	$s6, $s6, 1 	#przesun s5
+	addi	$t8, 1
+	beq	$t8, 32, nextword
+	# sprawdz, czy nie trzeba zaladowac nowego do s6
 
-	# jesli sie nie udalo a byly przedtem sukcesy
-	beqz	$s6, blunsucont
-	#wroc do miejsca ostatniego sukcesu w buforze
-	move	$t1, $t6	# odzyskaj wskazanie na bajt
-	lbu	$s1, ($t1)	# odzyskaj bajt
-	move	$s5, $t7	# odzyskaj licznik
-	move	$t3, $t8	# odzyska maske
-blunsucont:
-	li	$s6, 0 		# kontrolnie zeruj licznik
-	li	$t4, 0
-	lbu	$s2, ($t5)	# wroc do poczatku sygnatury
-	srlv	$s2,$s2,$s5	# przesun o tyle ile jest maska	
-blcont: 
-	# kontynuuj petle
-	srl	$t3,$t3, 1 	# przesun maske, zeby badac nastepny bit
-	addi	$s5, 1		# inkrementuj licznik przesuniec 
-	b byteloop 		# salto
-
-byteloopend:
-	# koniec petli, wez nastepny bajt ciagu przeszukiwanego
-	addi	$t1, 1
 	b loop
 
-bytesuccess:
-# jesli to pierwszy sukces, zapamietujemy, gdzie sie zdarzyl, zeby moc wrocic
-# jesli okaze sie, ze to lipa
-	beqz	$s6, firstsuc
-bscont:
-# jesli znalezlismy pasujace bajty, nie przesuwamy sygnatury,
-# dzieki temu bedziemy badac nastepny bit. Zwiekszamy tylko licznik
-# zeby jesli jednak dopasowanie sie nie uda, moc wrocic do odpowiedniej pozycji
-	addi	$s6, 1
-	addi	$t4, 1
-# kontroluj, czy nie skonczyla sie sygnatura (dlugosc okreslona w sigsize)
-	la	$t9, sigsize		# 
-	lbu	$s7, ($t9)
-	beq	$s7, $s6, success
-# kontroluj, czy nie skonczyl sie juz bajt sygnatury. Jesli tak, zaladuj nastepny
-	beq	$t4, 8, nextbyte#j jesli doszedl do 8 (caly bit sie przesunal)
-	b blcont
+nextword:
+	addi	$t1, 4
+	lw	$s6, ($t1)
+	b loop
 
-nextbyte:
-	li	$t4, 0		# wyzeruj licznik przesuniecia sygnatury
-	addi	$t2, 1		# wez nastepny bit z sygnatury
-	lbu	$s2, ($t2)	# zaladuj go do s2
-	b	blcont		# wroc
-firstsuc:
-	move	$t6, $t1	# zapisz miejsce w buforze
-	move	$t7, $s5	# zapisz licznik petli bajtu
-	move	$t8, $t3	# zapisz maske
-	b	bscont
 
 success:
-
+	sub	$t1, 8		# przesun dopoczatku
 #wypisz ktory to bit	
 	la	$a1, buffer # zapisz poczatkowy bit 
-	move	$v0, $t6	# wpsiz bajt na ktorym znaleziono
+	move	$v0, $t1	# wpsiz bajt na ktorym znaleziono
 	subu	$v0, $v0, $a1	# odejmij
-	mulou	$v0, $v0, 8	# ile to bitow?
-	addu	$v0, $v0, $t7
+	mulou	$v0, $v0, 32	# ile to bitow?
+	addu	$v0, $v0, $t8
 	jr	$ra
 
-#successloop:
-#	addi	$t6, 1
-#	addi	$t5, 1 #nastepny znak
-#	lbu	$t8, ($t6)
-#	beq	$t8, 0, loopend #kon, zwr
 loopend:
 	li	$v0, -1
 	jr	$ra
@@ -151,17 +126,15 @@ main:
 	li	$v0, 16
 	syscall
 
-#plik wczytany, salto
-	la	$a0, buffer 
-	la	$a1, sig
-        la	$a2, bytemask	
-	li	$s6, 0
-	li	$t4, 0
-	jal	findsig
-	
+#plik wczytany, 
 	li	$v0, 4		# wypisz komunikat
 	la	$a0, bajt
 	syscall
+
+	la	$a0, buffer 
+	la	$a1, sig
+	la	$a2, sigsize		# 
+	jal	findsig
 
 	move	$a0, $v0	# wypisz wynik
 	li	$v0, 1
