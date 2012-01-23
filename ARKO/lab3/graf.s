6b3929542ebbe0761ba8c128fbb95b9189b3e032
Jakub Kol <jakub@jjkrol.pl>
2012-01-23 21:53:42 +0100

    ARKO: lab3, wersja do oddania na laborkach, nadal rysuje czarne kreski

--- ARKO/lab3/graf.s
+++ ARKO/lab3/graf.s
@@ -1,29 +1,31 @@
 section .text
 global putLine
 
+%idefine	ypos	[ebp + 16]
+
+%idefine	xstart	[ebp - 4]
+%idefine	xstop	[ebp - 8]
+%idefine	xlength	[ebp - 12]
+%idefine	width	[ebp - 16]
+%idefine	horzB	[ebp - 20]
+%idefine	horzG	[ebp - 24]
+%idefine	horzR	[ebp - 28]
+%idefine	dltB	[ebp - 32]
+%idefine	dltG	[ebp - 36]
+%idefine	dltR	[ebp - 40]
 
 putLine:
 push ebp
 mov	ebp, esp
 
-
-
-
 mov	edx,	[ebp+8]
 mov	[lfrgb], edx 
-
 mov	edx,	[ebp+12]
 mov	[rtrgb], edx
-
-mov	edx, [ebp+16]	;y
-mov	[y], edx
-
 mov	edx, [ebp+20]	; pInfo
 mov	[imageStruct], edx
-
 mov	edx, [ebp+24]	; lfdlt
 mov	[lfdlt], edx
-
 mov	edx, [ebp+28]	; ;lfrgb
 mov	[rtdlt], edx
 
@@ -32,65 +34,52 @@ mov	eax,	[lfrgb]
 mov	edx,	[eax+12]
 add	edx, 0x8000
 sar	edx, 16
-mov	[xstart], edx
+mov	xstart, edx
 
 ; get xstop
 mov	eax,	[rtrgb]
 mov	edx,	[eax+12]
 add	edx, 0x8000
 sar	edx, 16
-mov	[xstop], edx
+mov	xstop, edx
 
 ; width of the line drawn (in bytes)
-mov 	ecx, [xstart] 	;xstart
-mov 	edx, [xstop] 	;xstop
+mov 	ecx, xstart 	;xstart
+mov 	edx, xstop 	;xstop
 sub	edx, ecx
 cmp	edx, 0
 jne	continue	; if xlength == 0, xlenngth = 1
-add	edx, 2		; 1!
+inc	edx
 continue:
-mov	[xlength], edx
+mov	xlength, edx
 inc	edx
 imul	edx, 3
-;add	edx, 6			dac czy nie?
-mov	[width], edx
+mov	width, edx
 
 ;; horizontal line parameters
 mov	eax,	[lfrgb]
 mov	edx,	[eax]
-mov	[horzR],edx
+mov	horzR,edx
 mov	edx,	[eax+4]
-mov	[horzG],edx
+mov	horzG,edx
 mov	edx,	[eax+8]
-mov	[horzB],edx
+mov	horzB,edx
 
+mov	esi,	0
 
+dltloop:
 mov	edx,	[rtrgb]
-mov	eax,	[edx]
-mov	ecx,	[horzR]
-mov	ebx,	[xlength]
+mov	eax,	[edx+8+esi*4]
+mov	ecx,	[ebp-20+esi*4]
+mov	ebx,	xlength
 sub	eax,	ecx
 cdq
 idiv	ebx	
-mov	[dltR],	eax
+mov	[ebp-32+esi*4],	eax
 
-mov	edx,	[rtrgb]
-mov	eax,	[edx+4]
-mov	ecx,	[horzG]
-mov	ebx,	[xlength]
-sub	eax,	ecx
-cdq
-idiv	ebx
-mov	[dltG],	eax
-;
-mov	edx,	[rtrgb]
-mov	eax,	[edx+8]
-mov	ecx,	[horzB]
-mov	ebx,	[xlength]
-sub	eax,	ecx
-cdq
-idiv	ebx			; eax / ebx
-mov	[dltB],	eax
+dec	esi
+cmp	esi, -3
+jnb	dltloop
 
 ; get line width in bytes
 mov	edx, [imageStruct]
@@ -98,64 +87,47 @@ mov	ecx, [edx]
 imul	ecx, 3		; in bytes
 add	ecx, 3
 sar	ecx, 2
-sal	ecx, 2
-mov	[lineWidth], ecx
+sal	ecx, 2		; line width in ecx
 
 ; get current offset
-mov 	edx, [lineWidth]
-mov	ecx, [y]
+mov	edx, ypos
 imul	ecx, edx	
-mov	eax, [xstart]
+mov	eax, xstart
 imul	eax, 3
 add	ecx, eax
-sub	ecx, 3
-mov	[offset], ecx
+sub	ecx, 3		; eksp
+mov	edx, ecx	; offset in bytes
 
 ; prepare for loop
 mov	ecx, 0
 mov	eax, [imageStruct]
-mov	edx, [eax+8]	;load image
-mov	eax, [offset]
-add	edx, eax
+mov	ebx, [eax+8]	;load image
+add	ebx, edx
 
-;; drawing loop
+; drawing loop
 loop:
 
-; RED
-; draw
-mov	eax, 	[horzR]
-sar	eax, 16
-mov	[edx+ecx], eax
-; increment
-mov	eax, 	[horzR]
-mov	ebx,	[dltR]
-add	eax,	ebx
-mov	[horzR],eax
+mov	esi,	0
+mov	edx, 	ebx
+add	edx, 	ecx
 
-; GREEN
+rgbloop:
 ; draw
-mov	eax, 	[horzG]
+mov	eax, 	[ebp-28+esi*4]
 sar	eax, 16
-mov	[edx+ecx+1], eax
-; incremenet
-mov	eax, 	[horzG]
-mov	ebx,	[dltG]
-add	eax,	ebx
-mov	[horzG],eax
-
-; BLUE
-; draw
-mov	eax, 	[horzB]
-sar	eax, 16
-mov	[edx+ecx+2], eax
+mov	[edx+esi], eax
 ; increment
-mov	eax, 	[horzB]
-mov	ebx,	[dltB]
-add	eax,	ebx
-mov	[horzB],eax
+mov	eax, 	[ebp-28+esi*4]
+add	eax,	[ebp-40+esi*4]
+mov	[ebp-28+esi*4],eax
+
+inc	esi
+cmp	esi, 3
+jb	rgbloop
 
 add	ecx, 3
-cmp	ecx, [width]
+
+cmp	ecx, width
 jb	loop
 
 
@@ -164,6 +136,7 @@ jb	loop
 mov	esi,	0
 mov	eax,	[lfdlt]
 mov	ecx,	[lfrgb]
+
 lfrgbloop:
 mov	edx,	[eax+esi*4]
 mov	ebx, 	[ecx+esi*4] 
@@ -178,6 +151,7 @@ jb	lfrgbloop
 mov	esi,	0
 mov	eax,	[rtdlt]
 mov	ecx,	[rtrgb]
+
 rtrgbloop:
 mov	edx,	[eax+esi*4]
 mov	ebx,	[ecx+esi*4] 
@@ -189,7 +163,7 @@ jb	rtrgbloop
 
 ; exit
 
-mov 	eax, [dltR] 
+mov 	eax, dltR 
 
 mov 	ebp, esp
 pop 	ebp
@@ -197,26 +171,9 @@ ret
 
 section .data
 imageStruct	dd 	0
-width		dd 	0
-lineWidth 	dd 	0
-xlength		dd	0
 
 lfrgb		dd	0
 rtrgb		dd	0
 lfdlt		dd	0
 rtdlt		dd	0
 
-xstart		dd 	0
-xstop		dd	0
-y		dd	0
-
-rgbStruct	dd	0
-horzR		dd	0
-horzG		dd	0
-horzB		dd	0
-
-dltStruct	dd	0
-dltR		dd	0
-dltG		dd	0
-dltB		dd	0
-offset		dd	0
